15b92a4f7fb843c710f9e666d6e8575ceb203d39
Swarnendu0123 <clickswarnendu123@gmail.com>
2023-08-18 16:54:03 +0530

    ...

---
+++ app.js
@@ -0,0 +1,37 @@
+let url = "https://api.genderize.io/?name="
+let input = document.querySelector("input");
+let btn = document.querySelector("button");
+let gender = document.querySelector(".gender")
+let probability = document.querySelector(".probability")
+let name = document.querySelector(".name")
+
+
+let showData = () => {
+    let oldUrl = url
+    url = url.concat(input.value)
+    console.log(url);
+    fetch(url)
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            name.innerText = data.name;
+            gender.innerText = data.gender
+            probability.innerText = data.probability * 100 + "%"
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    url = oldUrl
+}
+
+
+btn.addEventListener("click", () => {
+    showData()
+})
+
+input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        showData()
+    }
+})
